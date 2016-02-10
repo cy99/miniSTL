f6c134dc6aa7f637d2eb2e426eeeb1e553b9a18b
owtotwo <owtotwo@163.com>
2016-02-10 11:09:22 +0800

    pair.h done!

--- mini_pair.h
+++ mini_pair.h
@@ -5,18 +5,64 @@
  */
 
 #ifndef __MINI_STL_PAIR_H
-#define __MINI_STL_PAIR_H		// Working...
+#define __MINI_STL_PAIR_H		// Done.
 
-#include <utility>
 
 
 namespace ministl {
 
-#if 0
-template <typename Key, typename Value>
-class pair : public std::pair<Key, Value> {};
-#endif // 0
-using std::pair;
+template <typename T1, typename T2>
+struct pair {
+
+// Member types
+	typedef T1 first_type;
+	typedef T2 second_type;
+
+// Member variables
+	first_type first;
+	second_type second;
+
+// Member functions
+	pair() {}
+
+	pair(const first_type& a, const second_type& b) : first(a), second(b) {}
+
+	template <typename U, typename V>
+	pair(const pair<U, V>& pr) : first(pr.first), second(pr.second) {}
+
+	pair& operator=(const pair& pr) {
+		first = pr.first;
+		second = pr.second;
+		return *this;
+	}
+
+// Relational operators
+	bool operator==(const pair& rhs) const {
+		return this->first == rhs.first && this->second == rhs.second;
+	}
+
+	bool operator<(const pair& rhs) const {
+		return (this->first < rhs.first) 
+					|| (!(rhs.first < this->first) && this->second < rhs.second);
+	}
+
+	bool operator!=(const pair& rhs) const {
+		return !(*this == rhs);
+	}
+
+	bool operator>(const pair& rhs) const {
+		return rhs < *this;
+	}
+
+	bool operator<=(const pair& rhs) const {
+		return !(*this > rhs);
+	}
+
+	bool operator>=(const pair& rhs) const {
+		return !(*this < rhs);
+	}
+};
+
 
 }// namespace ministl
 
